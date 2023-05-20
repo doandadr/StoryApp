caec68733d37786bcf3c597f7280b06095107c4b
Doanda37Rahma <ddoanda@gmail.com>
2023-05-21 01:51:26 +0700

    feat: finish login

--- app/src/main/java/com/dicoding/doanda/storyapp/LoginActivity.kt
+++ app/src/main/java/com/dicoding/doanda/storyapp/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.dicoding.doanda.storyapp
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -12,11 +15,13 @@ import androidx.lifecycle.ViewModelProvider
 import com.dicoding.doanda.storyapp.databinding.ActivityLoginBinding
 import com.dicoding.doanda.storyapp.helper.LoginViewModelFactory
 import com.dicoding.doanda.storyapp.helper.SessionPreferences
+import com.dicoding.doanda.storyapp.helper.UserEntity
 import com.dicoding.doanda.storyapp.models.LoginViewModel
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
 
 class LoginActivity : AppCompatActivity() {
 
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
     private lateinit var binding: ActivityLoginBinding
     private lateinit var loginViewModel: LoginViewModel
 
@@ -26,45 +31,80 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val pref = SessionPreferences.getInstance(dataStore)
-        // cal login api get response
         loginViewModel = ViewModelProvider(this, LoginViewModelFactory(pref))
             .get(LoginViewModel::class.java)
 
         loginViewModel.loginResponse.observe(this) { loginResponse ->
             if (loginResponse?.error == false) {
-                loginViewModel.saveIsLoggedIn(true)
-                loginViewModel.saveBearerToken(loginResponse.loginResult?.token.toString())
+                val user = UserEntity(
+                    loginResponse.loginResult?.name.toString(),
+                    true,
+                    "Bearer ${loginResponse.loginResult?.token}",
+                )
+                loginViewModel.saveUser(user)
+            } else {
+                Toast.makeText(this@LoginActivity, loginResponse?.message ?: "Email or password incorrect", Toast.LENGTH_SHORT).show()
+            }
+        }
 
-                Toast.makeText(this@LoginActivity, "Login successful", Toast.LENGTH_SHORT).show()
-
+        loginViewModel.getUser().observe(this) { user ->
+            val bearerToken = user.bearerToken
+            if (bearerToken != "") {
+                Toast.makeText(this@LoginActivity, "Login succesful", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(intent)
                 finish()
-            } else {
-                Toast.makeText(this@LoginActivity, loginResponse?.message ?: "error", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        loginViewModel.isLoading.observe(this) { isLoading ->
+            showLoading(isLoading)
         }
 
         binding.btnLogin.setOnClickListener { view ->
             if (view.id == R.id.btn_login) {
-                // get fields
                 val email = binding.edLoginEmail.text.toString()
                 val password = binding.edLoginPassword.text.toString()
-
-                // verify fields TODO put in custom edittext
-
                 loginViewModel.loginRequest(email, password)
             }
         }
 
-        binding.btnToRegister.setOnClickListener { view ->
-            if (view.id == R.id.btn_to_register) {
-                val intent = Intent(this, RegisterActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-                finish()
-            }
+        binding.btnToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+
+        playAnimation()
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.ivLoginLogo, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val title = ObjectAnimator.ofFloat(binding.tvLoginTitle, View.ALPHA, 1f).setDuration(500)
+        val desc = ObjectAnimator.ofFloat(binding.tvLoginDesc, View.ALPHA, 1f).setDuration(500)
+        val edEmail = ObjectAnimator.ofFloat(binding.edLoginEmail, View.ALPHA, 1f).setDuration(500)
+        val edRegister = ObjectAnimator.ofFloat(binding.edLoginPassword, View.ALPHA, 1f).setDuration(500)
+        val btnLogin = ObjectAnimator.ofFloat(binding.btnLogin, View.ALPHA, 1f).setDuration(500)
+        val btnToRegister = ObjectAnimator.ofFloat(binding.btnToRegister, View.ALPHA, 1f).setDuration(500)
+
+        AnimatorSet().apply {
+            playSequentially(title, desc, edEmail, edRegister, btnLogin, btnToRegister)
+            startDelay = 500
+        }.start()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.pbLogin.visibility = View.VISIBLE
+        } else {
+            binding.pbLogin.visibility = View.GONE
         }
     }
 }
--- app/src/main/java/com/dicoding/doanda/storyapp/models/LoginViewModel.kt
+++ app/src/main/java/com/dicoding/doanda/storyapp/models/LoginViewModel.kt
@@ -1,14 +1,11 @@
 package com.dicoding.doanda.storyapp.models
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.dicoding.doanda.storyapp.helper.SessionPreferences
+import com.dicoding.doanda.storyapp.helper.UserEntity
 import com.dicoding.doanda.storyapp.network.ApiConfig
 import com.dicoding.doanda.storyapp.network.LoginResponse
-import com.dicoding.doanda.storyapp.network.LoginResult
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,25 +13,25 @@ import retrofit2.Response
 
 class LoginViewModel(private val pref: SessionPreferences) : ViewModel() {
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
     private val _loginResponse = MutableLiveData<LoginResponse?>()
     val loginResponse: LiveData<LoginResponse?> = _loginResponse
 
-    fun saveBearerToken(token: String) {
+    fun getUser(): LiveData<UserEntity> = pref.getUser().asLiveData()
+    fun saveUser(user: UserEntity) {
         viewModelScope.launch {
-            pref.saveBearerToken("Bearer $token")
-        }
-    }
-
-    fun saveIsLoggedIn(isLoggedIn: Boolean) {
-        viewModelScope.launch {
-            pref.saveIsLoggedIn(isLoggedIn)
+            pref.saveUser(user)
         }
     }
 
     fun loginRequest(email: String, password: String) {
+        _isLoading.value = true
         val client = ApiConfig.getApiService().login(email, password)
         client.enqueue(object : Callback<LoginResponse> {
             override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                _isLoading.value = false
                 if (response.isSuccessful) {
                     val responseBody = response.body()
                     if (responseBody != null) {
@@ -47,6 +44,7 @@ class LoginViewModel(private val pref: SessionPreferences) : ViewModel() {
             }
 
             override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                _isLoading.value = false
                 Log.e(TAG, "onFailure: ${t.message}")
             }
         })
