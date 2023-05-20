aa01d220e26f762186ce65c3b7fe0b5623c787e8
Doanda37Rahma <ddoanda@gmail.com>
2023-05-21 01:51:58 +0700

    feat: add custom edittext

---
+++ app/src/main/java/com/dicoding/doanda/storyapp/helper/EmailEditText.kt
@@ -0,0 +1,45 @@
+package com.dicoding.doanda.storyapp.helper
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.appcompat.widget.AppCompatEditText
+
+class EmailEditText : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (Patterns.EMAIL_ADDRESS.matcher(text.toString()).matches()) {
+                    error = null
+                } else {
+                    error = "Must be a valid email address"
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+}
